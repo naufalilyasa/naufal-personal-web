0762a0d728418b4c37778d20d67118717baf1a88
naufalilyasa <naufal.ilyasa@gmail.com>
2025-02-16 20:51:01 +0700

    make CRUD project

--- controllers/controller-v1.js
+++ controllers/controller-v1.js
@@ -19,16 +19,114 @@ async function renderProjectsPage(req, res) {
   res.render("projects", { projects: getProjects });
 }
 
-// RENDER CREATE PROJECTS PAGE
+// RENDER EDIT PAGE
+async function renderEditProjectPage(req, res) {
+  const id = req.params.id;
+
+  const existingProject = await Project.findOne({
+    where: { id: id },
+  });
+
+  if (existingProject === null) {
+    res.redirect("/404-not-found");
+  } else {
+    res.render("project-edit", { project: existingProject });
+  }
+}
+
+// RENDER CREATE PROJECT PAGE
 function renderProjectCreatePage(req, res) {
   res.render("project-create");
 }
 
-function createProject(req, res) {}
+// CREATE PROJECT
+async function createProject(req, res) {
+  let {
+    startProjectDate,
+    endProjectDate,
+    nodeJs,
+    nextJs,
+    reactJs,
+    typescript,
+  } = req.body;
+  const { projectName, description } = req.body;
+
+  const projects = {
+    projectName: projectName,
+    startProjectDate: startProjectDate,
+    endProjectDate: endProjectDate,
+    technologyNodeJs: Boolean(nodeJs),
+    technologyNextJs: Boolean(nextJs),
+    technologyReactJs: Boolean(reactJs),
+    technologyTypescript: Boolean(typescript),
+    description: description,
+    image: "/img/class.webp",
+  };
+
+  await Project.create(projects)
+    // .then((res) => console.log(res))
+    .catch((error) => console.error(error));
+  res.redirect("/projects");
+}
+
+// DELETE PROJECT
+async function deleteProject(req, res) {
+  const id = req.params.id;
+
+  const deletedProject = await Project.destroy({
+    where: { id: id },
+  });
+
+  res.redirect("/projects");
+}
+
+// UPDATE PROJECT
+async function updateProject(req, res) {
+  const id = req.params.id;
+  let {
+    projectName,
+    startProjectDate,
+    endProjectDate,
+    nodeJs,
+    nextJs,
+    reactJs,
+    typescript,
+    description,
+  } = req.body;
+
+  const editedProject = {
+    projectName: projectName,
+    startProjectDate: startProjectDate,
+    endProjectDate: endProjectDate,
+    technologyNodeJs: Boolean(nodeJs),
+    technologyNextJs: Boolean(nextJs),
+    technologyReactJs: Boolean(reactJs),
+    technologyTypescript: Boolean(typescript),
+    description: description,
+  };
+
+  const updatedProject = await Project.update(editedProject, {
+    where: { id: id },
+  })
+    .then((res) => console.log(res))
+    .catch((error) => console.error(error));
+
+  res.redirect("/projects");
+}
+
+// RENDER 404 NOT FOUND PAGE
+function render404NotFoundPage(req, res) {
+  res.render("404-not-found");
+}
 
 module.exports = {
   renderHomePage,
   renderContactPage,
   renderProjectsPage,
   renderProjectCreatePage,
+  renderEditProjectPage,
+  createProject,
+  deleteProject,
+  updateProject,
+  render404NotFoundPage,
 };
--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const {
   renderContactPage,
   renderProjectsPage,
   renderProjectCreatePage,
+  createProject,
+  deleteProject,
+  renderEditProjectPage,
+  render404NotFoundPage,
+  updateProject,
 } = require("./controllers/controller-v1.js");
+const { projectDuration } = require("./utils/projectDuration.js");
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views"));
@@ -24,18 +30,35 @@ hbs.registerPartials(__dirname + "/views/partials", function (err) {});
 hbs.registerHelper("equal", function (a, b) {
   return a === b;
 });
+hbs.registerHelper("projectDuration", projectDuration);
 
-// HOME PAGE
+// RENDER HOME PAGE
 app.get("/", renderHomePage);
 
-// CONTACT PAGE
+// RENDER CONTACT PAGE
 app.get("/contact", renderContactPage);
 
-// PROJECTS PAGE
+// RENDER PROJECTS PAGE
 app.get("/projects", renderProjectsPage);
 
-// CREATE PROJECT PAGE
+// RENDER CREATE PROJECT PAGE
 app.get("/project-create", renderProjectCreatePage);
+
+// RENDER EDIT PROJECT PAGE
+app.get("/project-edit/:id", renderEditProjectPage);
+
+// CREATE PROJECT
+app.post("/project-create", createProject);
+
+// DELETE PROJECT
+app.post("/project/:id", deleteProject);
+
+// UPDATE PROJECT
+app.post("/project-update/:id", updateProject);
+
+// 404 NOT FOUND PAGE
+app.get("*", render404NotFoundPage);
+app.get("/404-not-found", render404NotFoundPage);
 
 app.listen(port, () => {
   console.log(`app listening to port ${port}`);
