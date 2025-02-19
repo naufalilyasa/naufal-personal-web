60f094df6dd7ff1ce3087fb5d26647c676ff81b8
naufalilyasa <naufal.ilyasa@gmail.com>
2025-02-19 21:34:07 +0700

    add constraint fk blogs to users

---
+++ migrations/20250219143004-blogs-fk-to-user.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint("Blogs", {
+      fields: ["authorId"],
+      type: "foreign key",
+      name: "blogs_fk_to_users",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
--- models/user.js
+++ models/user.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Blog, {
+        foreignKey: "authorId",
+        as: "blogs",
+      });
     }
   }
   User.init(
