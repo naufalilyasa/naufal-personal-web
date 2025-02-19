afa08ee44942f70a6915eaf909bd6981f6526229
naufalilyasa <naufal.ilyasa@gmail.com>
2025-02-19 21:33:13 +0700

    create blog model

---
+++ models/blog.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Blog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Blog.belongsTo(models.User, {
+        foreignKey: "authorId",
+        as: "user",
+        onDelete: "SET NULL",
+        onUpdate: "CASCADE",
+      });
+    }
+  }
+  Blog.init(
+    {
+      authorId: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      image: DataTypes.STRING,
+      content: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Blog",
+    }
+  );
+  return Blog;
+};
