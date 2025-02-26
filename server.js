1abb5df2f1085602b02d8ab2e507a4d528db114b
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-02-27 05:52:17 +0700

    fix

--- server.js
+++ server.js
@@ -96,6 +96,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
       // secure: process.env.NODE_ENV === "production",
       secure: true,
